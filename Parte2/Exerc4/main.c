dc1d7aff8c3e7b1e8fce5c45bf8da791f450b7f7
João Leocádio <joaocarvalho388@gmail.com>
2020-10-20 14:46:57 +0100

    done!

--- Parte2/Exerc4/main.c
+++ Parte2/Exerc4/main.c
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
  * Author: joaoc
@@ -19,6 +13,19 @@
  */
 int main(int argc, char** argv) {
 
+    float preco1, preco2, preco3, total, desconto;
+    
+    preco1 = preco2 = preco3 = total = desconto = 0.0;
+    
+    puts("Introduza 3 preços: ");
+    scanf("%f%f%f", &preco1, &preco2, &preco3);
+    
+    desconto = (preco1 + preco2 + preco3) * 0.10;
+    
+    total = (preco1 + preco2 + preco3) - desconto;
+    
+    printf("Valor total: %.2f\n", total);
+    
     return (EXIT_SUCCESS);
 }
 
---
+++ Parte2/Exerc5/main.c
@@ -0,0 +1,23 @@
+
+/* 
+ * File:   main.c
+ * Author: joaoc
+ *
+ * Created on 16 de Outubro de 2020, 15:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int min, segundo;
+    
+    puts("Introduza a duracao em mm:ss: ");
+    scanf("%d%d", &min, &segundo);
+    return (EXIT_SUCCESS);
+}
+
